1c8730aaf1e6147e278d95afdadf5ebe81928c30
alla redko <alla.redko@jetbrains.com>
2019-12-25 10:27:05 +0300

    sample file for the Editor Basics lesson

---
+++ editor/actions.py
@@ -0,0 +1,1 @@
+print('Hello!' '\n' 'Press double Shift')
---
+++ editor/delete-line.py
@@ -0,0 +1,6 @@
+for n in range(1, 100):
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    print("The factorial of ", n, "is: ")
+    print(fact, "\n")
